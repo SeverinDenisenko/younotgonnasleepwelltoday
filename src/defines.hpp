894a83edfea370764cd45a6e179f9cb1197d834f
Severin Denisenko <81779013+SeverinDenisenko@users.noreply.github.com>
2024-02-18 15:15:11 +0300

    Basic setup

---
+++ src/defines.hpp
@@ -0,0 +1,28 @@
+#include <cstddef>
+#include <cstdint>
+
+#include <memory>
+#include <utility>
+
+namespace en
+{
+using u8  = uint8_t;
+using i8  = int8_t;
+using u16 = uint16_t;
+using i16 = int16_t;
+using u32 = uint32_t;
+using i32 = int32_t;
+using u64 = uint64_t;
+using i64 = int64_t;
+using f32 = float;
+using f64 = double;
+
+template <typename T>
+using uptr = std::unique_ptr<T>;
+
+template <typename T>
+using wptr = std::weak_ptr<T>;
+
+template <typename T>
+using sptr = std::shared_ptr<T>;
+} // namespace en
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,80 @@
 #include "raylib.h"
 
-int main(void)
-{
-    InitWindow(800, 450, "raylib [core] example - basic window");
+#include "defines.hpp"
 
-    while (!WindowShouldClose())
+namespace en
+{
+class IGame
+{
+public:
+    virtual void setup() noexcept    = 0;
+    virtual void update() noexcept   = 0;
+    virtual void shutdown() noexcept = 0;
+
+    virtual ~IGame() = default;
+};
+
+class IRunner
+{
+public:
+    virtual void run() noexcept = 0;
+
+    virtual ~IRunner() = default;
+};
+
+class Runner : public IRunner
+{
+public:
+    Runner(uptr<IGame> game)
+        : game_(std::move(game))
     {
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
     }
 
-    CloseWindow();
+    void run() noexcept override
+    {
+        game_->setup();
+
+        while (!WindowShouldClose()) {
+            game_->update();
+        }
+
+        game_->shutdown();
+    }
+
+private:
+    uptr<IGame> game_;
+};
+} // namespace en
+
+namespace ge
+{
+class Game : public en::IGame
+{
+public:
+    void setup() noexcept override
+    {
+        InitWindow(800, 450, "raylib [core] example - basic window");
+    }
+    void update() noexcept override
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    void shutdown() noexcept override
+    {
+        CloseWindow();
+    }
+};
+} // namespace ge
+
+
+int main(void)
+{
+    en::uptr<en::IGame> game    = std::make_unique<ge::Game>();
+    en::uptr<en::Runner> runner = std::make_unique<en::Runner>(std::move(game));
+    runner->run();
 
     return 0;
 }
-
